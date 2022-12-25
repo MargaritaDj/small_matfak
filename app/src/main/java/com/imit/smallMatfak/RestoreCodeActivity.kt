010b3819a936be78c9b8453e57585033ca4a08ca
Margarita <margaritad@mail.ru>
2022-12-25 12:11:45 +0600

    [+] changed ImageView to CardView for round corners of button shadow [+] eye icon for showing password changed alpha [+] added screen password recovery [+] added screen receiving a code from sms

---
+++ app/src/main/java/com/imit/smallMatfak/RestoreCodeActivity.kt
@@ -0,0 +1,13 @@
+package com.imit.smallMatfak
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RestoreCodeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restore_code)
+
+
+    }
+}
