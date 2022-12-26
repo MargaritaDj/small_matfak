90919c0f0f178af4d6b0009c2be2ae5e93842757
Margarita <margaritad@mail.ru>
2022-12-26 16:32:45 +0600

    [+] created package screens

--- app/src/main/java/com/imit/smallMatfak/screens/ForgotPasswordActivity.kt
+++ app/src/main/java/com/imit/smallMatfak/screens/ForgotPasswordActivity.kt
@@ -1,10 +1,12 @@
-package com.imit.smallMatfak
+package com.imit.smallMatfak.screens
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.imit.smallMatfak.MainActivity
+import com.imit.smallMatfak.R
 
 class ForgotPasswordActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/imit/smallMatfak/screens/NewPasswordActivity.kt
+++ app/src/main/java/com/imit/smallMatfak/screens/NewPasswordActivity.kt
@@ -1,9 +1,11 @@
-package com.imit.smallMatfak
+package com.imit.smallMatfak.screens
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.imit.smallMatfak.MainActivity
+import com.imit.smallMatfak.R
 
 class NewPasswordActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
