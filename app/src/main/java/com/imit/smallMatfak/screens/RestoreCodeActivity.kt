89ac72eb05abc4034bfe2642ec72d6cef3bf09ac
Margarita <margaritad@mail.ru>
2022-12-27 01:52:41 +0600

    [+] added auto switch focus cells code

--- app/src/main/java/com/imit/smallMatfak/screens/RestoreCodeActivity.kt
+++ app/src/main/java/com/imit/smallMatfak/screens/RestoreCodeActivity.kt
@@ -3,10 +3,12 @@ package com.imit.smallMatfak.screens
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.imit.smallMatfak.MainActivity
 import com.imit.smallMatfak.R
+import com.imit.smallMatfak.utils.UtilsView
 
 class RestoreCodeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +29,12 @@ class RestoreCodeActivity: AppCompatActivity() {
         buttonHome.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        val editTextCode1: EditText = findViewById(R.id.restore_activity_cell_code1)
+        val editTextCode2: EditText = findViewById(R.id.restore_activity_cell_code2)
+        val editTextCode3: EditText = findViewById(R.id.restore_activity_cell_code3)
+        val editTextCode4: EditText = findViewById(R.id.restore_activity_cell_code4)
+
+        UtilsView.switchCellsCode(editTextCode1, editTextCode2, editTextCode3, editTextCode4)
     }
 }
--- app/src/main/java/com/imit/smallMatfak/utils/UtilsView.kt
+++ app/src/main/java/com/imit/smallMatfak/utils/UtilsView.kt
@@ -9,7 +9,7 @@ import com.imit.smallMatfak.R
 
 object UtilsView {
 
-    fun startPhone(editText: EditText){
+    fun startPhone(editText: EditText) {
         val startPhone = "+7"
         editText.setOnFocusChangeListener { _, hasFocus ->
             if (editText.text.toString() == "" && hasFocus) {
@@ -17,26 +17,48 @@ object UtilsView {
             }
         }
         editText.addTextChangedListener {
-            if(editText.text.toString().length < 2 ||
-                !editText.text.toString().startsWith(startPhone)){
+            if (editText.text.toString().length < 2 ||
+                !editText.text.toString().startsWith(startPhone)
+            ) {
                 editText.setText(startPhone)
                 editText.setSelection(editText.text.toString().length)
             }
         }
     }
 
-    fun changePasswordVisibility(editText: EditText, button: ImageButton){
+    fun changePasswordVisibility(editText: EditText, button: ImageButton) {
         button.setOnClickListener {
-            if(button.tag == "1"){
-                 button.setBackgroundResource(R.drawable.icon_open_eye)
-                 editText.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            if (button.tag == "1") {
+                button.setBackgroundResource(R.drawable.icon_open_eye)
+                editText.transformationMethod = HideReturnsTransformationMethod.getInstance()
                 button.tag = "2"
             } else {
-                 button.setBackgroundResource(R.drawable.icon_close_eye)
-                 editText.transformationMethod = PasswordTransformationMethod.getInstance()
+                button.setBackgroundResource(R.drawable.icon_close_eye)
+                editText.transformationMethod = PasswordTransformationMethod.getInstance()
                 button.tag = "1"
             }
             editText.setSelection(editText.text.toString().length)
+        }
+    }
+
+    fun switchCellsCode(
+        editText1: EditText, editText2: EditText,
+        editText3: EditText, editText4: EditText
+    ) {
+        editText1.addTextChangedListener {
+            if (editText1.text.toString() != "") {
+                editText2.requestFocus()
+            }
+        }
+        editText2.addTextChangedListener {
+            if (editText2.text.toString() != "") {
+                editText3.requestFocus()
+            }
+        }
+        editText3.addTextChangedListener {
+            if (editText3.text.toString() != "") {
+                editText4.requestFocus()
+            }
         }
     }
 }
